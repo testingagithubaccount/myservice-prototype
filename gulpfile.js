ccb99f4480e2f1f7e291631ec152b8493f5dfe6d
Tim McLaren <timmclaren@mcshaman.com>
2018-07-17 12:52:58 +1000

    Include CSS prefixes for older versions of Safari

--- gulpfile.js
+++ gulpfile.js
@@ -11,62 +11,62 @@ var util = require('gulp-util');
 var babel = require("gulp-babel");
 
 var config = {
-  production: !!util.env.production
+	production: !!util.env.production
 };
 
 var autoprefixerOptions = {
-  browsers: ['last 2 versions', '> 5%', 'Firefox ESR']
+	browsers: ['last 2 versions', '> 5%', 'Firefox ESR', 'last 7 Safari versions']
 };
 
 gulp.task("js", function () {
-  return gulp.src("src/js/*.js")
-    .pipe(babel())
-    .pipe(gulp.dest("docs/js"));
+	return gulp.src("src/js/*.js")
+		.pipe(babel())
+		.pipe(gulp.dest("docs/js"));
 });
 
 
 gulp.task('sass', function () {
-  return gulp.src(['src/sass/main.scss', 'src/sass/myaccount.scss'])
-    .pipe(sourcemaps.init())
-    .pipe(sass({ includePaths: ['src/sass'] }))
-    .pipe(sass().on('error', sass.logError))
-    .pipe(config.production ? uglifycss({ "maxLineLen": 80 }) : util.noop())
-    .pipe(autoprefixer(autoprefixerOptions))
-    .pipe(config.production ? util.noop() : sourcemaps.write())
-    .pipe(gulp.dest('docs/css'))
-    .pipe(sassdoc())
-    .pipe(livereload());
+	return gulp.src(['src/sass/main.scss', 'src/sass/myaccount.scss'])
+		.pipe(sourcemaps.init())
+		.pipe(sass({ includePaths: ['src/sass'] }))
+		.pipe(sass().on('error', sass.logError))
+		.pipe(config.production ? uglifycss({ "maxLineLen": 80 }) : util.noop())
+		.pipe(autoprefixer(autoprefixerOptions))
+		.pipe(config.production ? util.noop() : sourcemaps.write())
+		.pipe(gulp.dest('docs/css'))
+		.pipe(sassdoc())
+		.pipe(livereload());
 });
 
 gulp.task('sass-prod', function () {
-  return gulp.src(['src/sass/main.scss', 'src/sass/myaccount.scss'])
-    .pipe(sourcemaps.init())
-    .pipe(sass({ includePaths: ['src/sass'] }))
-    .pipe(sass().on('error', sass.logError))
-    .pipe(uglifycss())
-    .pipe(autoprefixer(autoprefixerOptions))
-    .pipe(gulp.dest('docs/css'));
+	return gulp.src(['src/sass/main.scss', 'src/sass/myaccount.scss'])
+		.pipe(sourcemaps.init())
+		.pipe(sass({ includePaths: ['src/sass'] }))
+		.pipe(sass().on('error', sass.logError))
+		.pipe(uglifycss())
+		.pipe(autoprefixer(autoprefixerOptions))
+		.pipe(gulp.dest('docs/css'));
 });
 
 
 
 gulp.task('ejs', function () {
-  return gulp.src('views/**/*.ejs')
-    .pipe(livereload());
+	return gulp.src('views/**/*.ejs')
+		.pipe(livereload());
 });
 
 gulp.task('watch', function () {
-  livereload.listen();
-  gulp.watch('src/sass/*.scss', ['sass']);
-  gulp.watch('src/js/*.js', ['js']);
-  gulp.watch('views/**/*.ejs', ['ejs']);
+	livereload.listen();
+	gulp.watch('src/sass/*.scss', ['sass']);
+	gulp.watch('src/js/*.js', ['js']);
+	gulp.watch('views/**/*.ejs', ['ejs']);
 });
 
 gulp.task('server', function () {
-  nodemon({
-    'script': 'index.js',
-    'ignore': 'docs/js/*.js'
-  });
+	nodemon({
+		'script': 'index.js',
+		'ignore': 'docs/js/*.js'
+	});
 });
 
 gulp.task('serve', ['server', 'sass', 'js', 'watch']);
